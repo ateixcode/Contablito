65593c08cf1440af2740b49a5db2c33953921d8e
teixeira.alejandro.cebas@gmail.com <teixeira.alejandro.cebas@gmail.com>
2021-10-04 21:55:52 -0300

    Primera estructura HTML
    Html base, registro de productos + tabla + linkeo a bootsrap

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("register-product-button").addEventListener("click", function (event) {
+        event.preventDefault()
+    });
+});
