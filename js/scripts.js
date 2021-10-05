8287452939969f5326308c39c8efb7eab17cbaab
teixeira.alejandro.cebas@gmail.com <teixeira.alejandro.cebas@gmail.com>
2021-10-05 19:55:56 -0300

    com

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
-window.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("register-product-button").addEventListener("click", function (event) {
-        event.preventDefault()
-    });
+let idCount = 0;
+let products = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("register-product-button").onclick = () => {
+
+        let productName = document.getElementById("product-name").value;
+        let productDescription = document.getElementById("product-description").value;
+        let productsInStock = document.getElementById("products-in-stock").value;
+        let productCost = document.getElementById("product-cost").value;
+
+        idCount++;
+
+        let productObject = { 'productName': productName, 'productDescription': productDescription, 'productsInStock': productsInStock, 'productCost': productCost, 'id': idCount };
+        sessionStorage.setItem('product', productObject);
+
+        let productListTable = document.getElementById("product-list-table");
+
+        productListTable.innerHTML += `
+        <tr>
+            <td>` + productName + `</td>
+            <td>` + productDescription + `</td>
+            <td>` + productsInStock + `</td>
+            <td>` + productCost + `</td>
+            <td>` + idCount + `</td>
+        </tr>
+        `
+    }
 });
+
